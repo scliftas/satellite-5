0d918a6824d9f8426fb961c82147b58ee6a7fbef
Shaun Clift <scliftas@gmail.com>
2020-01-14 18:23:41 +0000

    Tidied up electron/server files

--- electron.js
+++ electron.js
@@ -1,66 +1,43 @@
 const electron = require('electron');
-const app = electron.app;
-const BrowserWindow = electron.BrowserWindow;
-require('./app');
+
+const { app, BrowserWindow } = electron.app;
+require('./src/server');
 
 const widevine = require('./lib/electron-widevinecdm/src/main');
- 
+
 widevine.load(app);
 
 let mainWindow;
 
-function createWindow () {
-    mainWindow = new BrowserWindow({
-        fullscreen: true,
-        webPreferences: {
-            nodeIntegration: false,
-            enableRemoteModule: false,
-            plugins: true,
-            partition: 'persist:satellite-5',
-            webviewTag: true,
-        },
-    });
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    fullscreen: true,
+    webPreferences: {
+      nodeIntegration: false,
+      enableRemoteModule: false,
+      plugins: true,
+      partition: 'persist:satellite-5',
+      webviewTag: true,
+    },
+  });
 
-    mainWindow.loadURL('http://localhost:3000/');
-    // mainWindow.loadURL(url.format({
-    //     pathname: path.join(__dirname, 'index.html'),
-    //     protocol: 'file:',
-    //     slashes: true
-    // }));
+  mainWindow.loadURL('http://localhost:3000/');
 
-    // Open the DevTools.
-    mainWindow.webContents.openDevTools();
-
-    // Emitted when the window is closed.
-    mainWindow.on('closed', function () {
-        // Dereference the window object, usually you would store windows
-        // in an array if your app supports multi windows, this is the time
-        // when you should delete the corresponding element.
-        mainWindow = null;
-    });
+  mainWindow.on('closed', () => {
+    mainWindow = null;
+  });
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
 app.on('ready', createWindow);
 
-// Quit when all windows are closed.
-app.on('window-all-closed', function () {
-    // On OS X it is common for applications and their menu bar
-    // to stay active until the user quits explicitly with Cmd + Q
-    if (process.platform !== 'darwin') {
-        app.quit();
-    }
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
 });
 
-app.on('activate', function () {
-    // On OS X it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
-    if (mainWindow === null) {
-        createWindow();
-    }
+app.on('activate', () => {
+  if (mainWindow === null) {
+    createWindow();
+  }
 });
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
---
+++ src/server.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const path = require('path');
+const players = require('./players');
+
+const app = express();
+const port = 3000;
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/:player/open', (req, res) => {
+  const playerName = req.params.player;
+  const player = players[playerName].src;
+
+  if (player === undefined) {
+    res.send(`Unable to find player: ${playerName}`);
+    return;
+  }
+
+  player.open();
+  res.send(`Opened ${playerName}`);
+});
+
+app.get('/:player/search', (req, res) => {
+  const playerName = req.params.player;
+  const player = players[playerName].src;
+
+  if (player === undefined) {
+    res.send(`Unable to find player: ${playerName}`);
+    return;
+  }
+
+  const content = req.query.q;
+
+  if (content === undefined) {
+    res.send('Please provide a search term');
+    return;
+  }
+
+  player.search(content);
+  res.send(`Searched ${playerName} for ${content}`);
+});
+
+app.get('/:player/play', (req, res) => {
+  const playerName = req.params.player;
+  const player = players[playerName].src;
+
+  if (player === undefined) {
+    res.send(`Unable to find player: ${playerName}`);
+    return;
+  }
+
+  const content = req.query.q;
+
+  if (content === undefined) {
+    res.send('Please provide a search term');
+    return;
+  }
+
+  player.play(content);
+  res.send(`Playing ${content} on ${playerName}`);
+});
+
+app.listen(port, () => console.log(`🛰   Satellite 5 is listening on port ${port}!`));
