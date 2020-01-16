e50585b7fe763442c561da0b8f361073d35886d7
Shaun Clift <scliftas@gmail.com>
2020-01-16 21:05:40 +0000

    Added react prop types

--- src/components/player.js
+++ src/components/player.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const Player = (props) => {
-  const { config } = props;
+  const {
+    name,
+    logo,
+  } = props;
 
-  return <img className="h-64 w-auto hover:h-96 m-6" src={config.logo} alt={config.name} />;
+  return <img className="h-64 w-auto hover:h-96 m-6" src={logo} alt={name} />;
+};
+
+Player.propTypes = {
+  name: PropTypes.string.isRequired,
+  logo: PropTypes.string.isRequired,
 };
 
 export default Player;
